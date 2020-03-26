2c60284621f58f8360167f632f47c05b0df70750
Fang Li <lifang_1030@hotmail.com>
2020-03-25 18:13:27 -0700

    deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn instaMini.wsgi --log-file -
--- instaMini/settings.py
+++ instaMini/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4z(7b*cwb)+@l#oopla^=xp-=k8a*aj!mosqwl+t$@vnmztp7d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'instaApp',
 ]
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
